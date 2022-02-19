a438e7c83fb059aa88d5a03149a38d42475b9ee5
rex <dafengyuxue@gmail.com>
2022-02-20 00:20:14 +0100

    add validators

---
+++ app/user/validators.py
@@ -0,0 +1,50 @@
+"""
+Customized validators
+"""
+import re
+
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.utils.translation import gettext_lazy as _
+
+from .models import User
+
+def validate_username(username):
+    if len(username) < 2 or len(username) > 128:
+        raise ValidationError(
+            _(f'{username} is expected to have at least two characters, but no more than 128 characters'),
+            params = {'username': username}
+        )
+
+def validate_password(password):
+    """
+    password is valid iff
+    1. consisting of 8 characters
+    2. no more than 32 characters
+    2. having at least a capital letter, a small letter and a number
+    """
+    password_re = re.compile(
+        r'(?=.*[0-9])'
+        r'(?=.*[a-z])'
+        r'(?=.*[A-Z])'
+        r'.{8, 32}'
+    )
+    if not password_re.fullmatch(password):
+        raise ValidationError(
+            _(f'The length of password is betweeen 8 and 32;\n\
+                Password contains number, uppercase and lowercase'),
+            params = {'password': password}
+        )
+
+def validate_user_email(email):
+    """customized email validator"""
+    # if compatible with email format
+    validate_email(email)
+    # if email does exist in the database
+    if email in User.objects.all().values_list('email', flat=True):
+        raise ValidationError(
+            _(f'The email has been registered'),
+            params = {'email': email}
+        )
+
+    
