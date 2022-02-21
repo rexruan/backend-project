c5f241401ff067e30bc33f3ccbf693c794c478e7
rex <dafengyuxue@gmail.com>
2022-02-21 22:27:18 +0100

    change name

--- app/user/tests/models.py
+++ app/user/tests/models.py
@@ -5,17 +5,13 @@ from django.test import TestCase
 from ..models import User
 # Create your tests here.
 
-class UserTestCase(TestCase):
-    
-    def test_users_creation(self):
-        User.objects.create()
-    
+class UserTestCase(TestCase): 
 
     def test_model_str(self):
         user = User.objects.create(
-            username='Eriksson',
+            username='username',
             password='Passw0rd',
             email='email@example.se'
         )
-        
+ 
         self.assertEqual(str(user), "Eriksson")
