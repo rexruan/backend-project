8b67091297cc2c1c71e8c141c165b3a65b21add5
rex <dafengyuxue@gmail.com>
2022-03-15 10:08:52 +0100

    add secrect key

--- app/settings/__init__.py
+++ app/settings/__init__.py
@@ -3,7 +3,8 @@ import os
 # from .local import *
 from .beta import *
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'test'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DATABASES['default']['USER']     = os.environ.get('POSTGRES_USER')
 DATABASES['default']['PASSWORD'] = os.environ.get('POSTGRES_PASSWORD')
